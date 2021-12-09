096b6325837eafc50799a87ff192da582fe2a4ad
jstephend-sun <johnstephen.degillo@sun-asterisk.com>
2021-12-09 14:00:37 +0800

    FollowLogs migration and Model

--- backend/database/migrations/2021_12_09_053150_create_follow_logs_table.php
+++ backend/database/migrations/2021_12_09_053150_create_follow_logs_table.php
@@ -4,17 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFollowLogsTable extends Migration
-{
+class CreateFollowLogsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('follow_logs', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('from_id')->constrained('users');
+            $table->foreignId('to_id')->constrained('users');
+
             $table->timestamps();
         });
     }
@@ -24,8 +26,7 @@ class CreateFollowLogsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('follow_logs');
     }
 }
