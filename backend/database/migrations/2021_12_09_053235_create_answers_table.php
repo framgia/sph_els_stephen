99a00337642ca3da958795267868840cc6219e16
jstephend-sun <johnstephen.degillo@sun-asterisk.com>
2021-12-09 14:05:12 +0800

    Answers migration and Model

--- backend/database/migrations/2021_12_09_053235_create_answers_table.php
+++ backend/database/migrations/2021_12_09_053235_create_answers_table.php
@@ -4,17 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAnswersTable extends Migration
-{
+class CreateAnswersTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('quiz_log_id')->constrained();
+            $table->foreignId('choice_id')->constrained();
+
             $table->timestamps();
         });
     }
@@ -24,8 +26,7 @@ class CreateAnswersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('answers');
     }
 }
