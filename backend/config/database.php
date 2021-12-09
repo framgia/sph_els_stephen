e516f2527ccc3c59844369dfecfdd2e95e9a528a
jstephend-sun <johnstephen.degillo@sun-asterisk.com>
2021-12-09 16:34:35 +0800

    Bugfix for database config

--- backend/config/database.php
+++ backend/config/database.php
@@ -38,12 +38,12 @@ return [
         // jaws db mysql
         'jawsdb' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('JAWSDB_URL'),
+            'host' => env('JAWSDB_HOST', '127.0.0.1'),
+            'port' => env('JAWSDB_PORT', '3306'),
+            'database' => env('JAWSDB_DATABASE', 'els'),
+            'username' => env('JAWSDB_USERNAME', 'root'),
+            'password' => env('JAWSDB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
