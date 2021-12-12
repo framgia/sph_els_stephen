fb8bff4f4ed4c1699a2b508f9eb99a9ef669bb1f
jstephend-sun <johnstephen.degillo@sun-asterisk.com>
2021-12-12 19:32:34 +0800

    Configure quizzes resources endpoint and logic (index, create, edit, delete)

---
+++ backend/app/Http/Controllers/QuizController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quiz;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Resources\QuizResource;
+
+class QuizController extends Controller {
+    public function index() {
+        return QuizResource::collection(Quiz::paginate(5));
+    }
+
+    public function store() {
+        $attrs = $this->validateQuiz();
+
+        $q = Quiz::create($attrs);
+        return new QuizResource($q);
+    }
+
+    public function show($id) {
+        return new QuizResource(Quiz::findOrFail($id));
+    }
+
+    public function update(Quiz $quiz) {
+        $attrs = $this->validateQuiz($quiz);
+        $quiz->fill($attrs);
+        if ($quiz->isClean()) {
+            return $this->errorResponse("No changes detected");
+        }
+        $quiz->save();
+        return new QuizResource($quiz);
+    }
+
+    public function destroy(Quiz $quiz) {
+        $quiz->delete();
+        return response()->json(
+            ['data' => ["message" => 'Quiz "$quiz->title" deleted successfully.'], "code" => 200],
+            200
+        );
+    }
+
+    protected function validateQuiz(?Quiz $quiz = null) {
+        $quiz ??= new Quiz();
+
+        return request()->validate([
+            'title' => [!$quiz->exists ? 'required' : '', Rule::unique('quizzes')->ignore($quiz)],
+            'description' => [!$quiz->exists ? 'required' : '']
+        ]);
+    }
+}
