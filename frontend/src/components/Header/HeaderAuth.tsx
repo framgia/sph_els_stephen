d52a82fdcebc77af2a2490ede189982c81cc9cdc
JStephenD <johnstephen.degillo@sun-asterisk.com>
2022-01-06 18:23:41 +0800

    removed unnecessary wrapper

--- frontend/src/components/Header/HeaderAuth.tsx
+++ frontend/src/components/Header/HeaderAuth.tsx
@@ -5,30 +5,26 @@ const HeaderAuth = () => {
   const [cookies, setCookies] = useCookies();
 
   return !cookies.user ? (
-    <>
-      <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-        <Link
-          to="/signin"
-          className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-        >
-          Sign in
-        </Link>
-        <Link
-          to="/signup"
-          className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-        >
-          Sign up
-        </Link>
-      </div>
-    </>
+    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+      <Link
+        to="/signin"
+        className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+      >
+        Sign in
+      </Link>
+      <Link
+        to="/signup"
+        className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+      >
+        Sign up
+      </Link>
+    </div>
   ) : (
-    <>
-      <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-        <p className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-          Welcome Back
-        </p>
-      </div>
-    </>
+    <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+      <p className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
+        Welcome Back
+      </p>
+    </div>
   );
 };
 
