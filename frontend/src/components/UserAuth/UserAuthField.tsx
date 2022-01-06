91337d8488be452a78dba8626bc128cd723bf9e6
John Stephen Degillo <95029803+jstephend-sun@users.noreply.github.com>
2022-01-06 13:30:14 +0800

    [SELS-TASK] Integrate user sign in and signup markup and api (#20)
    * [SELS-TASK] Integrate user sign in and signup markup and api
    
    * Error message passed as props

--- frontend/src/components/UserAuth/UserAuthField.tsx
+++ frontend/src/components/UserAuth/UserAuthField.tsx
@@ -2,33 +2,35 @@ import React from 'react';
 
 interface UserAuthFieldProps {
   label: string;
-  name: string;
+  register?: {};
   type?: string;
   rounded?: string;
   autocomplete?: string;
+  errormsg?: string;
 }
 
 export const UserAuthField = ({
   label,
-  name,
+  register = {},
   type = 'text',
   rounded = 't-md',
   autocomplete,
+  errormsg = '',
 }: UserAuthFieldProps) => {
   return (
     <div>
-      <label htmlFor={name} className="sr-only">
+      <label htmlFor={label} className="sr-only">
         {label}
       </label>
       <input
-        id={name}
-        name={name}
+        {...register}
         type={type}
-        autoComplete={autocomplete ? autocomplete : name}
+        autoComplete={autocomplete ? autocomplete : label}
         required
         className={`appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-${rounded} focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm`}
         placeholder={label}
       />
+      <span className="text-red-500 italic">{errormsg}</span>
     </div>
   );
 };
--- frontend/src/components/UserAuth/UserSignUp.tsx
+++ frontend/src/components/UserAuth/UserSignUp.tsx
@@ -1,5 +1,9 @@
 import { LockClosedIcon } from '@heroicons/react/outline';
-import React from 'react';
+import { AxiosError, AxiosResponse } from 'axios';
+import React, { useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
 import {
   UserAuthButton,
   UserAuthField,
@@ -7,37 +11,138 @@ import {
   UserAuthHeader,
   UserAuthLoginExtra,
 } from '.';
+import backend from '../../api/backend';
+
+interface FormInput {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
+const formValidation = {
+  name: {
+    required: {
+      value: true,
+      message: 'This field is required.',
+    },
+    maxLength: {
+      value: 255,
+      message: 'Email field max character up to 255 only.',
+    },
+  },
+  email: {
+    required: {
+      value: true,
+      message: 'This field is required.',
+    },
+    maxLength: {
+      value: 255,
+      message: 'Email field max character up to 255 only.',
+    },
+  },
+  password: {
+    required: {
+      value: true,
+      message: 'This field is required.',
+    },
+    maxLength: {
+      value: 255,
+      message: 'Password field max character up to 255 only.',
+    },
+    minLength: {
+      value: 6,
+      message: 'Password field min character is 6.',
+    },
+  },
+  password_confirmation: {
+    required: {
+      value: true,
+      message: 'This field is required.',
+    },
+    maxLength: {
+      value: 255,
+      message: 'Password field max character up to 255 only.',
+    },
+    minLength: {
+      value: 6,
+      message: 'Password field min character is 6.',
+    },
+  },
+};
 
 export const UserSignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInput>();
+
+  const navigate = useNavigate();
+  const [isError, setIsError] = useState(false);
+  const [cookies, setCookies] = useCookies();
+
+  const onSubmit = (data: FormInput) => {
+    setIsError(false);
+    backend.get('/sanctum/csrf-cookie').then((csrf_response) => {
+      backend
+        .post('/api/users/', data)
+        .then((response: AxiosResponse) => {
+          setCookies('user', response.data.data);
+          setCookies('token', response.data.token);
+          navigate('/signin');
+        })
+        .catch((err: AxiosError) => {
+          setIsError(true);
+        });
+    });
+  };
+
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <UserAuthHeader>Register to Stephen's E-Learning System</UserAuthHeader>
 
-        <UserAuthForm action="#" method="POST">
+        <UserAuthForm handleSubmit={handleSubmit(onSubmit)}>
           <div className="rounded-md shadow-sm -space-y-px">
             <UserAuthField
               label="Full Name"
-              name="name"
+              register={{ ...register('name', formValidation.name) }}
               rounded="t-md rounded-b-md"
+              errormsg={errors.name?.message}
             />
           </div>
 
           <div className="rounded-md shadow-sm -space-y-px">
             <UserAuthField
               label="Email Address"
-              name="email"
+              register={{ ...register('email', formValidation.email) }}
               type="email"
               rounded="t-md"
+              errormsg={errors.email?.message}
             />
-            <UserAuthField label="Password" name="password" rounded="none" />
+            <UserAuthField
+              label="Password"
+              register={{ ...register('password', formValidation.password) }}
+              type="password"
+              rounded="none"
+              errormsg={errors.password?.message}
+            />
             <UserAuthField
               label="Password Confirmation"
-              name="password_confirmation"
+              register={{
+                ...register(
+                  'password_confirmation',
+                  formValidation.password_confirmation
+                ),
+              }}
+              type="password"
               rounded="b-md"
+              errormsg={errors.password_confirmation?.message}
             />
           </div>
 
+          <div>{isError ? 'Invalid Input' : ''}</div>
           <UserAuthButton label="Register" />
         </UserAuthForm>
       </div>
