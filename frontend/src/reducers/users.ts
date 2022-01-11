8e05e0247cf9b22ba486815918cb4ad5ef27ddbe
JStephenD <johnstephen.degillo@sun-asterisk.com>
2022-01-11 14:32:58 +0800

    better variable names

--- frontend/src/reducers/users.ts
+++ frontend/src/reducers/users.ts
@@ -2,14 +2,14 @@ import { Action, ActionTypes, UserData, UsersData } from '../actions';
 
 const followUserFunc = (state: UsersData, action: Action) => {
   let users = state.data || [];
-  let to_follow = users.find((u) => u.id === action.payload);
+  let to_follow = users.find((user) => user.id === action.payload);
 
   return {
-    data: users.map((u) => {
-      if (u.id == to_follow?.id) {
+    data: users.map((user) => {
+      if (user.id == to_follow?.id) {
         return { ...to_follow, is_following: !to_follow.is_following };
       }
-      return u;
+      return user;
     }),
   };
 };
