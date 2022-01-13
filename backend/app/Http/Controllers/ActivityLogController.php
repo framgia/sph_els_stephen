ef090f3285405c916c86e0914e339473fbf0fe69
John Stephen Degillo <95029803+jstephend-sun@users.noreply.github.com>
2022-01-13 17:00:31 +0800

    [SELS-TASK] Configure activity logs api (#45)

--- backend/app/Http/Controllers/ActivityLogController.php
+++ backend/app/Http/Controllers/ActivityLogController.php
@@ -2,8 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ActivityLogController extends Controller {
-    //
+    public function index() {
+        return $this->getUserWithActivityLogs(Auth::id());
+    }
+
+    public function show($id) {
+        return $this->getUserWithActivityLogs($id);
+    }
+
+    protected function getUserWithActivityLogs($id) {
+        return response()->json([
+            "data" => User::where('id', $id)->with([
+                'quiz_logs.log',
+                'following.log',
+                'followers.log'
+            ])->first()
+        ]);
+    }
 }
