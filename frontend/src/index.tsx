c5dce23a038c06fee0879a3862d5d5c3ac1edd24
John Stephen Degillo <95029803+jstephend-sun@users.noreply.github.com>
2022-01-14 11:58:34 +0800

    [SELS-TASK] add cookie provider to app (#48)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,13 +7,16 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { reducers } from './reducers';
+import { CookiesProvider } from 'react-cookie';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
