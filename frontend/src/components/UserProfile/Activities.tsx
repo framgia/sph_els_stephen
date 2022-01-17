9965080f2ea31608b88e412c520035a123f5fc2b
JStephenD <johnstephen.degillo@sun-asterisk.com>
2022-01-17 17:24:58 +0800

    remove unused code

--- frontend/src/components/UserProfile/Activities.tsx
+++ frontend/src/components/UserProfile/Activities.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
