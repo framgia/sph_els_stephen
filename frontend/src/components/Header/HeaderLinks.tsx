4b42e64e23f587e39a18191cd1d7093b0ddd819b
JStephenD <johnstephen.degillo@sun-asterisk.com>
2022-01-19 13:38:19 +0800

    remove comments and fix accidental removed code

--- frontend/src/components/Header/HeaderLinks.tsx
+++ frontend/src/components/Header/HeaderLinks.tsx
@@ -8,36 +8,12 @@ const HeaderLinks = () => {
 
   return (
     <Popover.Group as="nav" className="hidden md:flex space-x-10">
-      {/* <HeaderDropDown
-        label="Quizzes"
-        dropDownItems={solutions}
-        callToActions={callsToAction}
-      /> */}
-
-      <>
-        <HeaderItem label="Quizzes" href="/quizzes" />
-        <HeaderItem label="Users" href="/users" />
-      </>
-
-      {/* <HeaderDropDown label="Quizzes" dropDownItems={solutions}>
-        <div>
-          <h3 className="text-sm tracking-wide font-medium text-gray-500 uppercase">
-            Recent Posts
-          </h3>
-          <ul role="list" className="mt-4 space-y-4">
-            {recentPosts.map((post) => (
-              <li key={post.id} className="text-base truncate">
-                <a
-                  href={post.href}
-                  className="font-medium text-gray-900 hover:text-gray-700"
-                >
-                  {post.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </HeaderDropDown> */}
+      {cookies?.user && (
+        <>
+          <HeaderItem label="Quizzes" href="/quizzes" />
+          <HeaderItem label="Users" href="/users" />
+        </>
+      )}
     </Popover.Group>
   );
 };
